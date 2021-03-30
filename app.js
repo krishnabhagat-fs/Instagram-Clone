fa5bbf6262ca06ff28b5845332b378a79243c595
Krishna Bhagat <krishnabhagat06@gmail.com>
2021-03-30 15:59:41 +0530

    Seting up Mongodb

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const mongoose = require('mongoose');
+//const {MONGOURI} = require('./keys');
+//console.log("This is Mongouri",MONGOURI);
+const uri = "mongodb+srv://Krish:7Jnip4Q74JTLcR0P@cluster0.og3ng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri,{
+    useNewUrlParser: true ,
+    useUnifiedTopology :true
+});
+mongoose.connection.on('connected',()=>
+{
+    console.log("Connected to mongo db Yeah");
+});
+mongoose.connection.on('error',(err)=>
+{
+    console.log("Connected to mongo db Yeah");
+});
+
+app.listen(PORT,()=>
+
+{
+    console.log('port is active at: ',PORT);
+})
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI="mongodb+srv://Krish:7Jnip4Q74JTLcR0P@cluster0.og3ng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
